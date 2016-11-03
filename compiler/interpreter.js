eaa450d9848bb5e62ed98f69c0762a9768ca11df
DESKTOP-FHKVQO3\taka <takashi.ssk1029@gmail.colm>
2016-11-03 17:18:55 +0900

    11/03

--- compiler/interpreter.js
+++ compiler/interpreter.js
@@ -5,11 +5,13 @@ function test(){
 		if(req.readyState==4){
 			if(req.status==200){
 				var ast = eval('(' + req.responseText + ')');
-				console.log('sucess')
-				console.log(ast)
+				console.log('sucess');
+				console.log(ast);
+				return ast;
 			}
 		}
 	}
 	req.open('GET','http://192.168.56.101:8880');
 	req.send(null);
 }
+
